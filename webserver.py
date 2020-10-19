8081b617b2010818da01779d98d57ec3ecdfa828
Tucker984 <55962220+Tucker984@users.noreply.github.com>
2020-10-18 19:07:24 -0500

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,13 +1,1 @@
-import http.server
-import socketserver
-import tkinter
-#config
-PORT = 8080
-Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    window = tkinter.Tk()
-window.title("Button GUI")
-button_widget = tkinter.Button(window,text="Welcome to DataCamp's Tutorial on Tkinter")
-button_widget.pack()
-tkinter.mainloop()
