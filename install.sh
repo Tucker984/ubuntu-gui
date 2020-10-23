8f96b85a502c8d7a7435eee46d84b9f52b22f050
Tucker984 <55962220+Tucker984@users.noreply.github.com>
2020-10-22 21:53:59 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash/
 sudo apt-get update
-sudo apt-get install python
-sudo apt-get install Apache2
-sudo a2enmod cgi
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt install python3.8
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
+sudo apt install python3-pip
+python -m pip install Django
