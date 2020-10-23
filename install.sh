8fa5cf438b43ebb7af5d1466191bad8d0857ae58
Tucker984 <55962220+Tucker984@users.noreply.github.com>
2020-10-23 10:01:17 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,3 @@
 #!/bin/bash/
 sudo apt-get update
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt-get update
-sudo apt install python3.8
-sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
-sudo apt install python3-pip
-python -m pip install Django
+sudo apt-get install perl
