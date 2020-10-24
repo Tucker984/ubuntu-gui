9cbb11070d7e1015ad36fa2e9124dda9ef33fc60
Tucker984 <55962220+Tucker984@users.noreply.github.com>
2020-10-23 19:39:12 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,16 @@ echo "installing npm/nodejs"
 sudo apt update
 sudo apt install nodejs
 sudo apt install npm
+echo "installing python"
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.8
+sudo apt update
+echo "installing pip"
+sudo apt-get update
+sudo apt-get -y install python3-pip
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
 echo "installing xterm module"
 npm install xterm
