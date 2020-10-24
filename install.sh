140e19cc48e1ea86dc9cd0a1b4e49c398e07d90c
Tucker984 <55962220+Tucker984@users.noreply.github.com>
2020-10-23 19:27:14 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash/
+echo "installing perl"
 sudo apt-get update
 sudo apt-get install perl
+echo "installing npm/nodejs"
+sudo apt update
+sudo apt install nodejs
+sudo apt install npm
+echo "installing xterm module"
+npm install xterm
