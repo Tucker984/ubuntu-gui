4fb1a75fc1285bd23bb929103106f4723b8965eb
Tucker984 <55962220+Tucker984@users.noreply.github.com>
2020-10-23 19:45:38 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,3 +19,8 @@ sudo apt-get -y install python3-pip
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
 echo "installing xterm module"
 npm install xterm
+echo "installing python-tk"
+sudo apt-get install python-tk
+echo "installing SQLite3"
+pip install bcrypt
+echo "install complete"
